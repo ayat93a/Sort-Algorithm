65bc8443461edb11ab9fe0296f58e89b1c752826
Ayat <alkayedayat93@gmail.com>
2022-05-19 08:12:12 +0300

    01

---
+++ merg_sort/merg_sort/merg_sort..py
@@ -0,0 +1,49 @@
+def Mergesort(arr):
+    n = len(arr)
+
+    if n > 1:
+        mid = n//2
+        left = arr[ : mid]
+        right = arr [mid :]
+
+        # sort the left side
+        Mergesort(left)
+
+        # sort the right side
+        Mergesort(right)
+       
+# merge the sorted left and right sides together
+        return Merg(left, right, arr)
+
+def Merg(left, right, arr):
+        i = 0
+        j = 0
+        k = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+                arr[k] = left[i]
+                i += 1
+            else:
+                arr[k] = right[j]
+                j +=  1
+
+            k += 1
+
+        while i < len(left):
+            arr[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            arr[k] = right[j]
+            j +=  1
+            k += 1
+
+        return arr
+
+if __name__ =='__main__':
+    print(Mergesort([8,4,23,42,16,15]))
+    print(Mergesort([20,18,12,8,5,-2]))
+    print(Mergesort([5,12,7,5,5,7]))
+    print(Mergesort([2,3,5,7,13,11]))
---
+++ merg_sort/tests/test_merg_sort.py
@@ -0,0 +1,5 @@
+
+
+
+def test_Mergesort(arr):
+    assert Mergesort([8,4,23,42,16,15]) == [4, 8, 15, 16, 23, 42]
